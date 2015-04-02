0d383c4ab9a829097bbc4fa9f1f5ce16507a4f8f
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-04-02 17:45:20 +0900

    Bugfix: not to display camera icon, unicode `f030`

--- Font-Awesome/Font-Awesome.swift
+++ Font-Awesome/Font-Awesome.swift
@@ -83,7 +83,7 @@ extension UIButton {
     func setFontAwesome(#fontAwesome: String, forState state: UIControlState) {
         let title = String.fontAwesome(unicode: fontAwesome)
         setTitle(title, forState: state)
-        let font = UIFont.fontAwesome(size: frame.height - 2) // use 1point to padding
+        let font = UIFont.fontAwesome(size: frame.height - 4) // use 1point to padding
         titleLabel!.font = font
     }
 
